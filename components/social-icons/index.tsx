9ad2c706029461cc37134fe4a1e9a7d7ebdc119f
Antonio Perez <3781123+antonioperez@users.noreply.github.com>
2025-07-16 16:10:24 -0700

    Update index.tsx

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -42,8 +42,7 @@ const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
   return (
     <a
       className="text-sm text-gray-500 transition hover:text-gray-600"
-      target="_blank"
-      rel="noopener noreferrer"
+      {...(kind !== 'mail' && { target: '_blank', rel: 'noopener noreferrer' })}
       href={href}
     >
       <span className="sr-only">{kind}</span>
