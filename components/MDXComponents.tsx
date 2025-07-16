86d159e002b3dba85b2c3150bb2ad355e76564f3
Antonio Perez <3781123+antonioperez@users.noreply.github.com>
2025-07-16 10:01:27 -0700

    Add Embed component and update site metadata
    Introduced an Embed component in MDXComponents to handle YouTube and generic embeds, and updated blog posts to use the new component. Updated site metadata for branding, contact, and analytics, removed the 'Tags' link from the header navigation, reordered tag data, added images to a blog post, and replaced the site logo.

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -6,6 +6,34 @@ import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 
+// Component to handle [embed] shortcodes
+const Embed = ({ url }) => {
+  // YouTube embed support
+  const youtubeMatch = url.match(
+    /(?:https?:\/\/(?:www\.|m\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/))([\w-]{11})/
+  )
+
+  if (youtubeMatch) {
+    return (
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${youtubeMatch[1]}`}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+    )
+  }
+
+  // Fallback: render as link
+  return <a href={url}>{url}</a>
+}
+
 export const components: MDXComponents = {
   Image,
   TOCInline,
@@ -13,4 +41,5 @@ export const components: MDXComponents = {
   pre: Pre,
   table: TableWrapper,
   BlogNewsletterForm,
+  Embed,
 }
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,27 +1,28 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  // color #BD0617
+  title: 'Software Survivor',
+  author: 'Antonio Perez',
+  headerTitle: 'Software Survivor',
+  description: 'A blog about software development, system design, and engineering leadership.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://softwaresurvivor.com',
+  siteRepo: 'https://github.com/antonioperez',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
+  //mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'aperez2541@gmail.com',
+  github: 'https://github.com/antonioperez',
+  //x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  medium: 'https://medium.com',
-  bluesky: 'https://bsky.app/',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/softwaresurvivor/',
+  // threads: 'https://www.threads.net',
+  // instagram: 'https://www.instagram.com',
+  // medium: 'https://medium.com',
+  // bluesky: 'https://bsky.app/',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
@@ -45,9 +46,9 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+    googleAnalytics: {
+      googleAnalyticsId: 'G-VZNXZXYJRC', // e.g. G-XXXXXXX
+    },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
