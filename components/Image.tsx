6ca0034b869c784feddcb5c51b20fa344cd7555d
Antonio Perez <3781123+antonioperez@users.noreply.github.com>
2025-07-15 22:47:28 -0700

    Update Image.tsx

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,7 +3,11 @@ import NextImage, { ImageProps } from 'next/image'
 const basePath = process.env.BASE_PATH
 
 const Image = ({ src, style, objectPosition = 'center top', ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} style={{ objectFit: 'cover', objectPosition, ...style }} {...rest} />
+  <NextImage
+    src={`${basePath || ''}${src}`}
+    style={{ objectFit: 'cover', objectPosition, ...style }}
+    {...rest}
+  />
 )
 
 export default Image
