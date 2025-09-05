730de16ea1649a11934341cb42cc1c95be1da52a
Antonio Perez <3781123+antonioperez@users.noreply.github.com>
2025-09-05 16:37:36 -0700

    Update Content Security Policy for Google Tag Manager
    Added https://*.googletagmanager.com and https://tagmanager.google.com to script-src in the Content Security Policy. Removed redundant frame-src entries for Google services to streamline the policy.

--- next.config.js
+++ next.config.js
@@ -7,17 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://*.googletagmanager.com https://tagmanager.google.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
   frame-src giscus.app
-  frame-src www.google-analytics.com
-  frame-src www.googletagmanager.com
-  frame-src www.youtube.com
-  frame-src www.google.com
 `
 
 const securityHeaders = [
